585f67be30d348e824ac9ac123a19a842b6079f7
Ghost-Vortex <m2206095@edu.misis.ru>
2026-02-23 14:40:57 +0300

    Update abstract_factory.py

--- abstract_factory.py
+++ abstract_factory.py
@@ -1,48 +1,26 @@
-class Chair:
-    def sit_on(self):
+class Button:
+    pass
+
+class WindowsButton(Button):
+    def paint(self):
+        print("Windows кнопка")
+
+class MacButton(Button):
+    def paint(self):
+        print("Mac кнопка")
+
+class GUIFactory:
+    def create_button(self):
         pass
 
-class ModernChair(Chair):
-    def sit_on(self):
-        return "Sitting on modern chair"
+class WindowsFactory(GUIFactory):
+    def create_button(self):
+        return WindowsButton()
 
-class VictorianChair(Chair):
-    def sit_on(self):
-        return "Sitting on victorian chair"
+class MacFactory(GUIFactory):
+    def create_button(self):
+        return MacButton()
 
-class Sofa:
-    def lie_on(self):
-        pass
-
-class ModernSofa(Sofa):
-    def lie_on(self):
-        return "Lying on modern sofa"
-
-class VictorianSofa(Sofa):
-    def lie_on(self):
-        return "Lying on victorian sofa"
-
-class FurnitureFactory:
-    def create_chair(self):
-        pass
-    def create_sofa(self):
-        pass
-
-class ModernFurnitureFactory(FurnitureFactory):
-    def create_chair(self):
-        return ModernChair()
-    def create_sofa(self):
-        return ModernSofa()
-
-class VictorianFurnitureFactory(FurnitureFactory):
-    def create_chair(self):
-        return VictorianChair()
-    def create_sofa(self):
-        return VictorianSofa()
-
-if __name__ == "__main__":
-    factory = ModernFurnitureFactory()
-    chair = factory.create_chair()
-    sofa = factory.create_sofa()
-    print(chair.sit_on())
-    print(sofa.lie_on())
+factory = WindowsFactory()
+button = factory.create_button()
+button.paint()
